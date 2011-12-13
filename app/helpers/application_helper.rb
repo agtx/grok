2af3ad8a4545222a4b065942ecd59335698f0546
Gilad Zohari <gzohari@gmail.com>
2011-12-13 14:13:11 +0200

    Added header/footer and a little styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def logo
+    image_tag('logo.png', :alt => "grok", :class => "round")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Grok::Application.routes.draw do
 
-
   
   root :to => "pages#home"
   match '/about', :to => "pages#about"
